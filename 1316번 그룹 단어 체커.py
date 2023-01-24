c15bc2003764553e3edd8c991b7f90fc5e79c17d
김현수 <goal6047@naver.com>
2023-01-24 19:50:11 +0900

    3rd commit

--- 1316번 그룹 단어 체커.py
+++ 1316번 그룹 단어 체커.py
@@ -1,10 +1,16 @@
 N =int(input())
-count = 0
+count = N
 for i in range(N):
     word = str(input())
-    for j in range(len(word)):
-        my_list=[]
+    my_list =[]
+    for j in range(len(word)-1):
         if word[j] == word[j+1]:
             my_list.append(word[j])
-            
         else:
+            if word[j+1] in my_list:
+                count-=1
+                break
+            else:
+                my_list.append(word[j])
+print(count)
+
---
+++ 2292번 벌집.py
@@ -0,0 +1,8 @@
+N = int(input())
+num =1
+i=0
+while num < N:
+    num += i*6
+    i +=1
+
+print(i)
